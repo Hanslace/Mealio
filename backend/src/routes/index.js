3d18fd197cb8fed26926291da281b89aaf286fb8
Haseeb Alvi <haseebalvi91@gmail.com>
2025-05-21 08:56:41 +0500

    backend settlement

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,20 +2,20 @@
 const router = require('express').Router();
 
 const authRoutes = require('./auth.routes');
-const userRoutes = require('./user.routes');
-const addressRoutes = require('./address.routes');
-const restaurantRoutes = require('./restaurant.routes');
-const menuItemRoutes = require('./menuItem.routes');
-const couponRoutes = require('./coupon.routes');
-const reviewRoutes = require('./review.routes');
-const orderRoutes = require('./order.routes');
-const deliveryRoutes = require('./delivery.routes');
-const chatRoutes = require('./chat.routes');
-const paymentRoutes = require('./payment.routes');
+// const userRoutes = require('./user.routes');
+// const addressRoutes = require('./address.routes');
+const restaurantRoutes = require('./restaurants.routes');
+const menuItemRoutes = require('./menuItems.routes');
+const couponRoutes = require('./coupons.routes');
+// const reviewRoutes = require('./review.routes');
+// const orderRoutes = require('./order.routes');
+// const deliveryRoutes = require('./delivery.routes');
+// const chatRoutes = require('./chat.routes');
+// const paymentRoutes = require('./payment.routes');
 const adminRoutes = require('./admin.routes');
-const deliveryPersonnelRoutes = require('./deliveryPersonnel.routes');
-const menuItemLikeRoutes = require('./menuItemLike.routes');
-const notificationRoutes = require('./notification.routes');
+// const deliveryPersonnelRoutes = require('./deliveryPersonnel.routes');
+// const menuItemLikeRoutes = require('./menuItemLike.routes');
+// const notificationRoutes = require('./notification.routes');
 
 // router.use('/notifications', notificationRoutes);
 // router.use('/likes', menuItemLikeRoutes);
